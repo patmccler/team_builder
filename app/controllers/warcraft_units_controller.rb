4b736e8af06783d0fac1b2f3675a735c9b8a9031
Pat McClernan <patmccler@gmail.com>
2020-06-30 09:14:30 -0400

    add new action and view for warcraft unit

--- app/controllers/warcraft_units_controller.rb
+++ app/controllers/warcraft_units_controller.rb
@@ -3,4 +3,9 @@ class WarcraftUnitsController < ApplicationController
     @roster = Roster.find_by(id: params[:roster_id])
     @units = @roster ? @roster.units : Unit.all
   end
+
+  def new
+    @unit = WarcraftUnit.new
+    @roster_id = params[:roster_id]
+  end
 end

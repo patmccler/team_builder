25a295bc06f36ad96e59098efabdba605ae2e1cc
Pat McClernan <patmccler@gmail.com>
2020-06-30 09:52:27 -0400

    fix typo in tft roster creation

--- app/helpers/roster_helper.rb
+++ app/helpers/roster_helper.rb
@@ -2,8 +2,8 @@ module RosterHelper
   def unit_path(roster, text = "See Units")
     if roster.game.unit_type == "WarcraftUnit"
       link_to text, roster_warcraft_units_path(@roster)
-    elsif roster.game.unit_tupe == "TeamFightTacticsUnit"
-      link_to text, roster_team_first_tactics_units_path(@roster)
+    elsif roster.game.unit_type == "TeamFightTacticsUnit"
+      link_to text, roster_team_fight_tactics_units_path(@roster)
     else
       "This roster doesn't have a recognizable unit type??"
     end

6ff67312bb0ce2b7030937f73bca6b5f3b8aa13c
Pat McClernan <patmccler@gmail.com>
2020-06-30 14:29:05 -0400

    add controller, route and new view for team_membership

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,13 @@ Rails.application.routes.draw do
 
   resources :rosters, only: [:create, :show, :edit, :update, :destroy] do
     resources :warcraft_units, only: [:index, :new, :create]
-    resources :team_fight_tactics_units
+    resources :team_fight_tactics_units, only: [:index, :new, :create]
   end
 
   resources :warcraft_units, only: [:create, :edit, :update, :show]
   resources :team_fight_tactics_units, only: [:create, :edit, :update, :show]
 
-  resources :teams
+  resources :teams do
+    resources :team_memberships, only: [:new, :create]
+  end
 end

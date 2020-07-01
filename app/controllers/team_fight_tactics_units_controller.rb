42e697702d5f64d13ffda1cd715e5f3e84907860
Pat McClernan <patmccler@gmail.com>
2020-07-01 00:06:38 -0400

    add update to tft units

--- app/controllers/team_fight_tactics_units_controller.rb
+++ app/controllers/team_fight_tactics_units_controller.rb
@@ -1,4 +1,6 @@
 class TeamFightTacticsUnitsController < ApplicationController
+  before_action :set_unit, only: [:edit, :update]
+
   def index
     @roster = Roster.find_by(id: params[:roster_id])
     @units = @roster ? @roster.units : Unit.all
@@ -14,12 +16,24 @@ class TeamFightTacticsUnitsController < ApplicationController
     @unit = TeamFightTacticsUnit.new(team_fight_tactics_unit_params)
     @roster = Roster.find(params[:roster_id])
     @unit.roster_memberships.build(roster: @roster) if @roster
-    @unit.game = roster.game
+    @unit.game = @roster.game
 
-    if unit.save
+    if @unit.save
       redirect_to @roster || @unit
     else
       render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @unit.update(team_fight_tactics_unit_params)
+      redirect_to roster_team_fight_tactics_units_path(@unit.rosters.last)
+    else
+      render :edit
     end
   end
 
@@ -27,5 +41,9 @@ private
 
   def team_fight_tactics_unit_params
     params.require(:team_fight_tactics_unit).permit(:name, :roster_ids)
+  end
+
+  def set_unit
+    @unit = TeamFightTacticsUnit.find(params[:id])
   end
 end

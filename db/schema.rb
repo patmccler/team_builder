81290f0cb496204ac192a01d19081e7e31f646b8
Pat McClernan <patmccler@gmail.com>
2020-07-01 08:21:18 -0400

    add user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_01_033628) do
+ActiveRecord::Schema.define(version: 2020_07_01_121555) do
 
   create_table "games", force: :cascade do |t|
     t.string "name"
@@ -58,6 +58,14 @@ ActiveRecord::Schema.define(version: 2020_07_01_033628) do
     t.integer "roster_id", null: false
     t.integer "team_size"
     t.index ["roster_id"], name: "index_teams_on_roster_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.boolean "admin", default: false
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "warcraft_units", force: :cascade do |t|

1eab2b3e3681a864f5b1b24d520401f4c4a5db1b
Pat McClernan <patmccler@gmail.com>
2020-07-02 02:54:40 -0400

    add validations for tft attrs

--- app/models/team_fight_tactics_unit.rb
+++ app/models/team_fight_tactics_unit.rb
@@ -1,4 +1,7 @@
 class TeamFightTacticsUnit < ApplicationRecord
   # Shared functionality in Unit module
   include Unit
+
+  validates :affiliation, presence: true, length: { maximum: 35 }
+  validates :combat_style, presence: true, length: { maximum: 35 }
 end

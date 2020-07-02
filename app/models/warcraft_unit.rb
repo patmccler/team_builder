e0bfe4b88d652d9f7087a26727c5de9fea69908a
Pat McClernan <patmccler@gmail.com>
2020-07-02 03:10:00 -0400

    fix rubocop violation

--- app/models/warcraft_unit.rb
+++ app/models/warcraft_unit.rb
@@ -2,7 +2,7 @@ class WarcraftUnit < ApplicationRecord
   # Shared functionality in Unit module
   include Unit
 
-  ROLE_VALUES = %w[Tank DPS Healer]
+  ROLE_VALUES = %w[Tank DPS Healer].freeze
 
   validates :buff, length: { maximum: 40 }
   validates :role, inclusion: { in: ROLE_VALUES }, presence: true

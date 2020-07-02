2d4fe45ec05b46118adf304ffc2e3d9b150c0f1b
Pat McClernan <patmccler@gmail.com>
2020-07-01 20:09:53 -0400

    add more user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,33 @@
 require "rails_helper"
 
 RSpec.describe User do
-  it "has a valid factory" do
-    expect(build(:user).valid?).to be(true)
+
+  describe "user validations" do
+    it "has a valid factory" do
+      expect(build(:user).valid?).to be(true)
+    end
+
+    it "must have unique username" do
+      create :user, username: "TEST"
+
+      expect(build(:user, username: "TEST").valid?).to be(false)
+    end
+
+    it "must have a username" do
+      user = build :user, username: nil
+
+      expect(user.valid?).to be(false)
+    end
   end
 
-  it "must have unique username" do
-    create :user, username: "TEST"
+  describe "user authentication" do
+    it "can only be authenticated with its password" do
+      password = "test_password"
+      user = create :user, password: password
 
-    expect(build(:user, username: "TEST").valid?).to be(false)
+
+      expect(user.authenticate(password)).to be(user)
+      expect(user.authenticate("NOT PASSWORD")).to be(false)
+    end
   end
-
 end

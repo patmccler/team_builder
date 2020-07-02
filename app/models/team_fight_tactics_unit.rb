225de5bc3153f96c81accc0e09abd72bc9be4d46
Pat McClernan <patmccler@gmail.com>
2020-07-01 23:55:45 -0400

    moved shared functionality between units into module

--- app/models/team_fight_tactics_unit.rb
+++ app/models/team_fight_tactics_unit.rb
@@ -1,22 +1,4 @@
 class TeamFightTacticsUnit < ApplicationRecord
-  belongs_to :game
-
-  has_many :team_memberships, as: :unit
-  has_many :teams, through: :team_memberships
-
-  has_many :roster_memberships, as: :unit
-  has_many :rosters, through: :roster_memberships
-
-  validates :name, presence: true
-  # Can I do this with scope directly
-  validate :name_unique_in_rosters, if: :name_changed?
-
-  scope :all_units_sharing_any_roster, ->(unit) {
-    joins(:roster_memberships).where(roster_memberships: { roster: unit.roster_memberships.map(&:roster_id) })
-  }
-
-  def name_unique_in_rosters
-    names = self.class.all_units_sharing_any_roster(self).pluck(:name).map(&:downcase)
-    errors.add(:name, "already taken in one of this unit's rosters") if names.include?(name.downcase)
-  end
+  # Shared functionality in Unit module
+  include Unit
 end

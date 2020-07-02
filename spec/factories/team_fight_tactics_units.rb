c227f31eb91ab1a9b7c295461e21fae5c891daa2
Pat McClernan <patmccler@gmail.com>
2020-07-02 02:56:47 -0400

    fix failing specs for new validations

--- spec/factories/team_fight_tactics_units.rb
+++ spec/factories/team_fight_tactics_units.rb
@@ -2,5 +2,7 @@ FactoryBot.define do
   factory :team_fight_tactics_unit do
     game
     name { "Poppy" }
+    affiliation { "Star Guardian" }
+    combat_style { "Vanguard" }
   end
 end
--- spec/factories/warcraft_units.rb
+++ spec/factories/warcraft_units.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :warcraft_unit do
     game
     name { "Protection Warrior" }
+    role { WarcraftUnit::ROLE_VALUES.first }
   end
 end

38d9d5d69026400beda6af7f1e16c56054c6ac7e
Pat McClernan <patmccler@gmail.com>
2020-07-02 00:43:13 -0400

    add commented out spec for tft units

--- spec/factories/roster_memberships.rb
+++ spec/factories/roster_memberships.rb
@@ -14,14 +14,10 @@ FactoryBot.define do
       end
     end
 
-    # roster and unit should point to same game
-    #after(:build) { |rm| rm.unit.game = rm.roster.game}
-
-    # TODO: Uncomment after implementing TFT UNIT FACTORY
-    # trait :for_team_fight_tactics_unit do
-    #   after(:build) do |rm, eval|
-    #     rm.unit = eval.unit || (build :team_fight_tactics_unit, game: rm.roster.game)
-    #   end
-    # end
+    trait :for_team_fight_tactics_unit do
+      after(:build) do |rm, eval|
+        rm.unit = eval.unit || (build :team_fight_tactics_unit, game: rm.roster.game)
+      end
+    end
   end
 end
